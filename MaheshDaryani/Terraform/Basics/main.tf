6095d5d2187c3c7953df33def675fd9771b70bfc
unknown <maheshdaryani07@gmail.com>
2025-10-27 23:54:18 +0530

    mahesh_folders

--- MaheshDaryani/Terraform/Basics/main.tf
+++ MaheshDaryani/Terraform/Basics/main.tf
@@ -1,5 +1,5 @@
 #creating a resource group with terraform resource block wiht only required arguments
-resource "azurerm_resource_group" "resource_group" {
-    name     = "dev-ssk-rg-001"
+resource "azurerm_resource_group" "mahesh_rg" {
+    name     = "dev-md-rg-001"
     location = "centralindia"
 }
