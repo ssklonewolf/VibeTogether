9c68bcd65055f6a35835c9af052c30eb2a10484b
Ashvani Pandey <ashvanipandey51@gmail.com>
2025-11-10 11:56:59 +0530

    adding helm

--- Ashvani/initial-folder-create.sh
+++ Ashvani/initial-folder-create.sh
@@ -2,3 +2,6 @@ mkdir -p Docker && touch Docker/Dockerfile
 mkdir -p Terraform && touch Terraform/main.tf
 mkdir -p kubernates && touch kubernates/pod.yaml
 mkdir -p Misc && touch Misc/notes.txt
+
+# Add VM folder with main.tf and variables.tf
+mkdir -p vm && touch vm/main.tf vm/variables.tf
